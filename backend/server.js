dc9befbfa0ba16068e2bf3a895c0609bd573bd54
Val-Ge <vg.dataengineer@gmail.com>
2024-07-18 13:44:17 +0100

    setup server.js

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+//create a web server with express
+const express = require('express');
+
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+const userRoutes = require('./routes/userRoutes');
+
+//load env variable into process.env
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+//use the express json middleware to parse json bodies of incoming requests
+app.use(express.json());
+
+//mount the user routes on the '/api/users' path
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
